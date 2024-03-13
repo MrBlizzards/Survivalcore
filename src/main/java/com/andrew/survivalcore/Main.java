f69df3dbbc734c4a227b9787045c04957a31776d
Blizzards <theblizzards0@gmail.com>
2024-03-13 17:37:00 -0500

    Registered all the new commands.

--- src/main/java/com/andrew/survivalcore/Main.java
+++ src/main/java/com/andrew/survivalcore/Main.java
@@ -1,10 +1,7 @@
 package com.andrew.survivalcore;
 
-import com.andrew.survivalcore.commands.MenuCommand;
-import com.andrew.survivalcore.commands.RankCommand;
-import com.andrew.survivalcore.listeners.MainMenuListener;
-import com.andrew.survivalcore.listeners.RankListener;
-import com.andrew.survivalcore.listeners.ScoreBoardListener;
+import com.andrew.survivalcore.commands.*;
+import com.andrew.survivalcore.listeners.*;
 import com.andrew.survivalcore.managers.NameTagManager;
 import com.andrew.survivalcore.managers.RankManager;
 import org.bukkit.Bukkit;
@@ -18,18 +15,28 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
 
-        // Register Listener
+        // Register Listeners
         Bukkit.getPluginManager().registerEvents(new RankListener(this), this);
         Bukkit.getPluginManager().registerEvents(new ScoreBoardListener(this), this);
         Bukkit.getPluginManager().registerEvents(new MainMenuListener(this), this);
         Bukkit.getPluginManager().registerEvents(new LauncherMenuListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new ServerListPingListener(this), this);
+
+        // Register Managers
+        rankManager = new RankManager(this);
+        nameTagManager = new NameTagManager(this);
 
         // Register Commands
         new RankCommand(this);
         new MenuCommand(this);
+        new CraftCommand(this);
+        new EnderChestCommand(this);
 
-        rankManager = new RankManager(this);
-        nameTagManager = new NameTagManager(this);
+        // Register Gamemode commands
+        new GamemodeCreative(this);
+        new GamemodeSurvival(this);
+        new GamemodeAdventure(this);
+        new GamemodeSpectator(this);
 
     }
 
