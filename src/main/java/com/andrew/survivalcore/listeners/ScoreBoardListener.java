a4e332efe617a9e1fd42938eda0e01457319dcde
Blizzards <theblizzards0@gmail.com>
2024-03-12 20:30:38 -0500

    Scoreboard listener class.

--- src/main/java/com/andrew/survivalcore/listeners/ScoreBoardListener.java
+++ src/main/java/com/andrew/survivalcore/listeners/ScoreBoardListener.java
@@ -1,7 +1,6 @@
 package com.andrew.survivalcore.listeners;
 
 import com.andrew.survivalcore.Main;
-import com.andrew.survivalcore.enums.RankEnum;
 import com.andrew.survivalcore.utils.ChatColorUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -11,51 +10,60 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scoreboard.*;
 
-import java.util.HashMap;
-
 public class ScoreBoardListener implements Listener {
 
     private Main main;
 
-    public ScoreBoardListener(Main main) {
-        this.main = main;
-
-    }
+    public ScoreBoardListener(Main main) { this.main = main; }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
+        createBoard(e.getPlayer());
+    }
 
-        Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
-        Objective obj = board.registerNewObjective("testboard", "dummy");
+    public void createBoard(Player player) {
+
+        // CREATE THE SCOREBOARD
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getNewScoreboard();
+        Objective obj = board.registerNewObjective("SurvivalCore-Rank", "dummy", ChatColorUtil.colorize("&b&lFrosted&f&lNetwork"));
         obj.setDisplaySlot(DisplaySlot.SIDEBAR);
-        obj.setDisplayName("Test Board!");
 
-        Player player = e.getPlayer();
+        // RIGHT UNDER THE TITLE
+        Score score = obj.getScore(ChatColorUtil.colorize(("&7&l-----------------")));
+        score.setScore(6);
 
-        Score website = obj.getScore("www.test-website");
+        // SPACE #2
+        Score space2 = obj.getScore("   ");
+        space2.setScore(5);
+
+        // PLAYER NAME
+        Score playerName = obj.getScore(ChatColorUtil.colorize("&7&lName: &7" + player.getName()));
+        playerName.setScore(4);
+
+        // RANK TEAM SYSTEM
+        Team team1 = board.registerNewTeam("team1");
+        String teamKey = ChatColor.GOLD.toString();
+
+        team1.addEntry(teamKey);
+        team1.setPrefix(ChatColorUtil.colorize("&7&lRank: "));
+        team1.setSuffix(main.getRankManager().getRank(player.getUniqueId()).getDisplay());
+
+        obj.getScore(teamKey).setScore(3);
+
+        // SPACE #1
+        Score space1 = obj.getScore(" ");
+        space1.setScore(2);
+
+        // WEBSTIE AT THE BOTTOM
+        Score website = obj.getScore(ChatColorUtil.colorize(("&7www.frostednetwork.com")));
         website.setScore(1);
 
-        Score space = obj.getScore(" ");
-        space.setScore(2);
-
-        Score name = obj.getScore(player.getName());
-        name.setScore(3);
-
-        Team playerRank = board.registerNewTeam("rank");
-        playerRank.addEntry(ChatColor.BOLD.toString() + "  ");
-        if (Bukkit.getOnlinePlayers().size() == 0) {
-            playerRank.setPrefix(main.getRankManager().getRank(player.getUniqueId()).getDisplay());
-        } else {
-            playerRank.setPrefix(main.getRankManager().getRank(player.getUniqueId()).getDisplay());
-        }
-        obj.getScore(ChatColor.BOLD.toString() + "  ").setScore(4);
-
-
+        // SETS THE SCOREBOARD
         player.setScoreboard(board);
 
-        https://www.spigotmc.org/wiki/making-scoreboard-with-teams-no-flicker/
-
-
     }
+
 }
+
 
