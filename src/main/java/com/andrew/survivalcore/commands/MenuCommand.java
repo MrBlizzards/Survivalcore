0b097e6c8de0c0759796f9b38aa80fae41d0b431
Blizzards <theblizzards0@gmail.com>
2024-03-12 20:29:34 -0500

    A menu command that opens up the main menu.

---
+++ src/main/java/com/andrew/survivalcore/commands/MenuCommand.java
@@ -0,0 +1,44 @@
+package com.andrew.survivalcore.commands;
+
+import com.andrew.survivalcore.GUI.MainMenuGUI;
+import com.andrew.survivalcore.Main;
+import com.andrew.survivalcore.managers.CommandManager;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class MenuCommand extends CommandManager {
+
+    private Main main;
+
+    public MenuCommand(Main main) {
+        super(
+                "menu",
+                new String[]{" "},
+                "Opens the main menu for the server",
+                "survivalcore.menu.main",
+                "Error! No permission node found for this command.");
+        this.main = main;
+    }
+
+    MainMenuGUI mainMenuGUI = new MainMenuGUI();
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+        // Main menu command /menu
+
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Only a player may execute this command.");
+            return;
+        }
+
+        player.openInventory(mainMenuGUI.getMainMenuInventory());
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, String[] args) {
+        return null;
+    }
+}
